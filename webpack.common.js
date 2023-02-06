472aa8fdbae17a9a0526bbdd95a20df270111896
ro-ssang <alakswk43@gmail.com>
2023-02-06 23:53:13 +0900

    feat: common path alias 추가

--- webpack.common.js
+++ webpack.common.js
@@ -9,6 +9,7 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
       '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@common': path.resolve(__dirname, 'src/common/'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@types': path.resolve(__dirname, 'src/types/'),
       '@utils': path.resolve(__dirname, 'src/utils/'),
